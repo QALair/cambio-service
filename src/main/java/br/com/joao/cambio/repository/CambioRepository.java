442bc3d55e58c428f0ae2b46a0670e6602443694
QALair <93294785+QALair@users.noreply.github.com>
2022-08-29 19:27:40 -0300

    first commit

---
+++ src/main/java/br/com/joao/cambio/repository/CambioRepository.java
@@ -0,0 +1,9 @@
+package br.com.joao.cambio.repository;
+
+import br.com.joao.cambio.model.Cambio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CambioRepository extends JpaRepository<Cambio, Long> {
+
+    Cambio findByFromAndTo(String from, String to);
+}
